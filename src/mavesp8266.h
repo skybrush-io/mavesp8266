010e70f1c1898930d36ef7dedf786130bafaaf8a
Guglielmo <gdguglie@gmail.com>
2020-05-20 16:21:47 +0200

    update build system: set version from platformio.ini + rename firmware with version and platform
    + ignore another platformio dir

--- src/mavesp8266.h
+++ src/mavesp8266.h
@@ -87,10 +87,7 @@ class MavESP8266PowerMgmt;
 //-- Uncomment this to simulate heartbeats even when the FC is powered down
 // #define SIMULATE_HEARTBEATS_WHEN_POWERED_OFF
 
-//-- TODO: This needs to come from the build system
-#define MAVESP8266_VERSION_MAJOR    1
-#define MAVESP8266_VERSION_MINOR    2
-#define MAVESP8266_VERSION_BUILD    3
+//-- The version is set from the build system (major, minor and build)
 #define MAVESP8266_VERSION          ((MAVESP8266_VERSION_MAJOR << 24) & 0xFF00000) | ((MAVESP8266_VERSION_MINOR << 16) & 0x00FF0000) | (MAVESP8266_VERSION_BUILD & 0xFFFF)
 
 //-- Debug sent out to Serial1 (GPIO02), which is TX only (no RX).
